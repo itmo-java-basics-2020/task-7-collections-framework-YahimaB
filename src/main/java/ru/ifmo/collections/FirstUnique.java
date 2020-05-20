2800671547913f0aecd5b39653c22b5f70e49ecb
Yaroslav <yaroslav.r.boyev@gmail.com>
2020-05-20 15:17:18 +0300

    Done

--- src/main/java/ru/ifmo/collections/FirstUnique.java
+++ src/main/java/ru/ifmo/collections/FirstUnique.java
@@ -1,19 +1,32 @@
 package ru.ifmo.collections;
 
+import java.util.LinkedHashMap;
+
 /**
  * Design a class which contains integers and returns first unique integer (in order of addition).
  * FirstUniqueTest can be used as an example.
  */
 public class FirstUnique {
+    private final LinkedHashMap<Integer, Boolean> linkedMap = new LinkedHashMap<>();
+
     public FirstUnique(int[] numbers) {
-        // TODO implement
+        for (var num : numbers) {
+            this.add(num);
+        }
     }
 
     public int showFirstUnique() {
-        throw new UnsupportedOperationException(); // TODO implement
+        for (var entry : linkedMap.entrySet()) {
+            if (entry.getValue()) {
+                return entry.getKey();
+            }
+        }
+
+        return -1;
     }
 
     public void add(int value) {
-        throw new UnsupportedOperationException(); // TODO implement
+        boolean isFirst = linkedMap.get(value) == null;
+        linkedMap.put(value, isFirst);
     }
 }
